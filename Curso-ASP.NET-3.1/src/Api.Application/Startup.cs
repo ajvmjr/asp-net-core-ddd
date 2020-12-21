45364dd0285d816c211f798cd8d89cfed6093046
Alex Moreira <alex.moreira@server.signainfo.com.br>
2020-12-21 16:25:29 -0300

    add swagger

--- Curso-ASP.NET-3.1/src/Api.Application/Startup.cs
+++ Curso-ASP.NET-3.1/src/Api.Application/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -26,10 +27,25 @@ namespace application
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
       ConfigureService.ConfigureDependenciesService(services);
       ConfigureRepository.ConfigureDependenciesRepository(services);
       services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "V1",
+          Title = "Curso de API com AspNetCore 3.1 - Na Prática",
+          Description = "Arquitetura DDD",
+          TermsOfService = new Uri("http://www.google.com"),
+          Contact = new OpenApiContact
+          {
+            Name = "Alex Junior",
+            Email = "alexjvm90@gmail.com",
+            Url = new Uri("http://www.google.com")
+          }
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +55,13 @@ namespace application
       {
         app.UseDeveloperExceptionPage();
       }
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com ASP NET 3.1 e Arquitetura DDD");
+        c.RoutePrefix = string.Empty;
+      });
 
       app.UseRouting();
 
